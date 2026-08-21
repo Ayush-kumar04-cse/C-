bde03b7e3115362ef2fa3f5ebf4788cce43559b9
Ayush kumar <ankitsabi04@gmail.com>
2026-08-21 18:06:14 +0530

    Remove extra part

--- Ayush.cpp
+++ Ayush.cpp
@@ -1,24 +1,15 @@
+
 # include <iostream>
 using namespace std;
-
-int main() {
-    cout << "C++" << endl;
-    cout << "Hello DSA";
-
+inline int max(int x,int y)
+{
+    return (x>y)?x:y;
+}
+int main()
+{
+    cout <<"max(20,10) : " <<max(20,10)<<endl;
+    cout<<"max(0,1000) : "<<max(0,1000)<<endl;
+    cout<<"max(100,1010) : "<<max(100,1010)<<endl;
+    
     return 0;
 }
-
-// # include <iostream>
-// using namespace std;
-// inline int max(int x,int y)
-// {
-//     return (x>y)?x:y;
-// }
-// int main()
-// {
-//     cout <<"max(20,10) : " <<max(20,10)<<endl;
-//     cout<<"max(0,1000) : "<<max(0,1000)<<endl;
-//     cout<<"max(100,1010) : "<<max(100,1010)<<endl;
-    
-//     return 0;
-// }
